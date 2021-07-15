84c6dba500c5ab9ae836f4ccd141a5c9ff3f0326
mediv0 <anonymousaxis@gmail.com>
2021-07-16 02:22:10 +0430

    feat: qol components

---
+++ components/noise.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="noise"></div>
+</template>
+
+<style lang="scss" scoped>
+@keyframes noise {
+    to {
+        background-position-y: 100%;
+    }
+}
+.noise {
+    pointer-events: none;
+    position: absolute;
+    inset: 0;
+    background: url(data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYGBgYHBgcICAcKCwoLCg8ODAwODxYQERAREBYiFRkVFRkVIh4kHhweJB42KiYmKjY+NDI0PkxERExfWl98fKf/2wBDAQYGBgYHBgcICAcKCwoLCg8ODAwODxYQERAREBYiFRkVFRkVIh4kHhweJB42KiYmKjY+NDI0PkxERExfWl98fKf/wAARCAAWACADASIAAhEBAxEB/8QAGAAAAwEBAAAAAAAAAAAAAAAAAAMHAQb/xAAzEAAAAwUCCQ0AAAAAAAAAAAAAAQIDBAUGVBEhEhMUF0KRlKLSFjIzQURRUlZydJLR4v/EABgBAAIDAAAAAAAAAAAAAAAAAAEGAAQF/8QAJhEAAAQDBwUAAAAAAAAAAAAAAAECBRZUoQMEERMkVpMVIVWRkv/aAAwDAQACEQMRAD8AgrZxgJtWpm+L6RWkV1/pCTcIDbYT4s7+8voUc47KBNV4cqLtwldkQG8pJL8m3e1SLsVXLa9lymFzNc8T0Dp8pEzyKA1TTWXCNyKAVi/l+RQFxuVdGUV7IgPTMUqdcoHsSBItuW1rLlMDMdvHOnpI5ZtPsRxzQidHfnK8QUU+ROlYa1gAMPpDdLJqH6IHntrV0BnBilK77wXnBiVKw3+IAACaW6WSDEDzOroP/9k=);
+    background-size: 7px auto;
+    opacity: 0.04;
+    -webkit-animation: noise 15s linear infinite;
+    animation: noise 15s linear infinite;
+    z-index: -1;
+}
+</style>
---
+++ components/player/graph.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="graph">
+        <div class="graph__line graph__line--1"></div>
+        <div class="graph__line graph__line--2"></div>
+        <div class="graph__line graph__line--3"></div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.graph {
+    display: flex;
+    align-items: flex-end;
+    height: 17px;
+    &__line {
+        width: 3px;
+        height: 100%;
+        background: var(--success-color);
+        margin: 0 2px;
+        animation: scale-graph 300ms linear alternate infinite both;
+        transform-origin: bottom;
+        &--1,
+        &--3 {
+            height: 17px;
+        }
+        &--2 {
+            height: 12px;
+        }
+    }
+}
+@keyframes scale-graph {
+    0% {
+        transform: scale(0.7);
+    }
+    100% {
+        transform: scale(1);
+    }
+}
+</style>
