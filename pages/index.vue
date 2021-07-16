a5171099c8a42ed2e71b332988ddbd77e5ab987d
mediv0 <anonymousaxis@gmail.com>
2021-07-16 22:33:08 +0430

    feat: main route

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="main_layout">
+        <player-title />
+        <player />
+        <slider />
+        <noise />
+    </div>
+</template>
+
+<script>
+import title from "@/components/title.vue";
+import player from "@/components/player/player.vue";
+import slider from "@/components/slider.vue";
+import noise from "@/components/noise.vue";
+export default {
+    components: {
+        playerTitle: title,
+        player,
+        slider,
+        noise
+    },
+
+    created() {
+        import("@/data/streams.json").then((module) => {
+            this.$store.commit("player/SET_PAYLIST", module.default);
+        })
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.main_layout {
+    text-align: center;
+}
+</style>
